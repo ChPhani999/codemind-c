e7a3cebaf26cbe445661fb9a760b5d236c45d9d7
CH SURYA PHANEENDRA <97582344+ChPhani999@users.noreply.github.com>
2022-01-19 16:47:21 +0530

    #include<stdio.h> main() {     int N;     scanf("%d",&N);     if(N%2==0)     {         printf("Even");     }     else     {         printf("Odd");     }     return 0; }

---
+++ Check if number is even or odd.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+main()
+{
+    int N;
+    scanf("%d",&N);
+    if(N%2==0)
+    {
+        printf("Even");
+    }
+    else
+    {
+        printf("Odd");
+    }
+    return 0;
+}
