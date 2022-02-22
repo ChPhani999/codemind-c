cd38b9e77bdd18c539e081bdeb68ed3096e53e47
SURYA PHANEENDRA CHAGANTI <97582344+ChPhani999@users.noreply.github.com>
2022-02-22 00:41:21 -0800

    Add files via upload

---
+++ _operators.c
@@ -0,0 +1,11 @@
+#include<stdio.h>//arithmatic operators
+main(){
+	int a,b;
+	printf("\nEnter a and b:");
+	scanf("%d%d",&a,&b);
+	printf("\naddition:%d",a+b);
+	printf("\nsubstraction:%d",a-b);
+	printf("\nmultiplication:%d",a*b);
+	printf("\nquotient:%d",a/b);
+	printf("\nremainder:%d",a%b);
+}
